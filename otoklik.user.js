8cf4ff7c2fbaec254481d02fac7bbba60b0b9341
jajanbocah <jajan.bocah@gmail.com>
2019-09-22 01:25:00 +0700

    Added anjay.info
    Rapiken tatanan link, sekalian tambah " anjay.info "

--- otoklik.user.js
+++ otoklik.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Klik
 // @namespace    http://www.emm-emm.com/
-// @version      1.3.2
+// @version      1.3.3
 // @description  Auto Klik Donlotan wakakaka
 // @author       eZee
 // @icon         https://graph.facebook.com/1750572307/picture
@@ -15,6 +15,7 @@
 // @match        *://*.greget.space/*
 // @match        *://*.siherp.com/*
 // @match        *://*.ahexa.com/*
+// @match        *://*.anjay.info/*
 
 // @match        *://davinsurance.com/*
 
@@ -37,23 +38,20 @@
 var fulldomen,domen, link, anu = 0, tipe, mod, jdl;
 var str, stt, a, b ,c;
 $(document).ready(function() {
-    //domen = document.domain;
     domen = document.documentURI;
     if(domen.match(/welcome.indihome.co.id/g)){
         link = $("div.background-box").find("a").attr("href");
+    // Url Shorter
     } else if(domen.match(/hexafile.net/g)){
         str = document.getElementsByTagName('script');
         stt = str[6].text.split('window.location="')[1].split('";');
         link = stt[0];
     } else if(domen.match(/tetew.info|greget.space|siherp.com|ahexa.com/g)){
         link = $("div.download-link").find("a").attr("href");
-    } else if(domen = domen.match(/zippyshare.com/g )){
-        link = $("a#dlbutton").attr("href");
-    } else if(document.documentURI.match(/drive.google.com\/file\/d\//g)){
-            a = document.documentURI.replace("/file/d/", "/uc?id=");
-            b = a.split("/view");
-            c = b[0].split("/edit");
-            link = c[0] + "&export=download";
+    } else if(domen.match(/anjay.info/g)){
+        str = document.getElementsByTagName('script');
+        stt = str[4].text.split("changeLink(){var a='")[1].split("';");
+        link = stt[0];
     } else if(document.documentURI.match(/davinsurance.com\//g)){
         if(document.documentURI.match(/\?id=/g)){
             tipe='klik';
@@ -73,6 +71,14 @@ $(document).ready(function() {
     } else if(document.documentURI.match(/safelinkreviewx.com/g )){
         var a = $("div.button").attr("onclick").replace("window.open('", "").replace("&pop=2', '_blank');", "");
         link = a.replace("decrypt2.safe", "decrypt.safe");
+    // Situs Download
+    } else if(domen = domen.match(/zippyshare.com/g )){
+        link = $("a#dlbutton").attr("href");
+    } else if(document.documentURI.match(/drive.google.com\/file\/d\//g)){
+            a = document.documentURI.replace("/file/d/", "/uc?id=");
+            b = a.split("/view");
+            c = b[0].split("/edit");
+            link = c[0] + "&export=download";
     } else if(document.documentURI.match(/zxcfiles.com/g )){
         var atext = $("span#download").text();
         if( atext == '' ){
